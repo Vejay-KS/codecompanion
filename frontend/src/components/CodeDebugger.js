a8ce1c06f78fe31bc31dbc08bec890b225909e44
Charan Krishna <charannori.0401@gmail.com>
2023-10-27 18:51:03 +1100

    Front end changes

---
+++ frontend/src/components/CodeDebugger.js
@@ -0,0 +1,35 @@
+﻿import React, { useState } from 'react';
+import axios from 'axios';
+import { API_URL } from '../constants';
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+axios.defaults.xsrfCookieName = "xcsrftoken";
+
+const CodeDebuggerFormUI = () => {
+  const [inputCode, setInputCode] = useState('');
+  const [codeCompletion, setCodeCompletion] = useState('');
+
+  const handleSubmit = async (e) => {
+    console.log("check");
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(API_URL, {
+        input_code: inputCode,
+      });
+      const { code_completion } = response.data;
+      setCodeCompletion(code_completion);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <input type="text" value={inputCode} onChange={(e) => setInputCode(e.target.value)} />
+      <button onClick={handleSubmit}>Debug Code</button>
+      <div>{codeCompletion}</div>
+    </div>
+  );
+};
+
+export default CodeDebuggerFormUI;
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,67 @@
+﻿import React from "react";
+import CodeOptimizerFormUI from "./CodeOptimizer";
+import CodeDebuggerFormUI from "./CodeDebugger";
+import CodeReviewerFormUI from "./CodeReviewer";
+import CommentGeneratorFormUI from "./CommentGenerator";
+
+const HomePage = () => {
+  const [activeSection, setActiveSection] = React.useState("code-optimiser");
+
+  const handleSectionChange = (sectionName) => {
+    setActiveSection(sectionName);
+  };
+
+  const renderSection = () => {
+    switch (activeSection) {
+      case "code-optimiser":
+        return <CodeOptimizerFormUI/>;
+      case "code-debugger":
+        return <CodeDebuggerFormUI/>;
+      case "code-reviewer":
+        return <CodeReviewerFormUI/>;
+      case "comment-generator":
+        return <CommentGeneratorFormUI/>;
+      default:
+        return <div>No section selected</div>;
+    }
+  };
+
+  return (
+    <div>
+      <h1>Code Companion</h1>
+      <nav>
+        <button onClick={() => handleSectionChange("code-optimiser")}>
+          Code Optimiser
+        </button>
+        <button onClick={() => handleSectionChange("code-debugger")}>
+          Code Debugger
+        </button>
+        <button onClick={() => handleSectionChange("code-reviewer")}>
+          Code Reviewer
+        </button>
+        <button onClick={() => handleSectionChange("comment-generator")}>
+          Comment Generator
+        </button>
+      </nav>
+      <main>{renderSection()}</main>
+    </div>
+  );
+};
+
+const CodeOptimiser = () => {
+  return <div>This is the Code Optimiser section</div>;
+};
+
+const CodeDebugger = () => {
+  return <div>This is the Code Debugger section</div>;
+};
+
+const CodeReviewer = () => {
+  return <div>This is the Code Reviewer section</div>;
+};
+
+const CommentGenerator = () => {
+  return <div>This is the Comment Generator section</div>;
+};
+
+export default HomePage;
